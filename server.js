7233927db3cdd96bcfccfc23039cc4e8e203f582
MBANS8A1 <seanmbng@aol.com>
2024-06-20 13:45:15 +0100

    added logic to package.json and to server.js to allow for connection to the mysql database; also added .env files

---
+++ server.js
@@ -0,0 +1,47 @@
+const Sequelize = require("sequelize");
+const environment = process.env.NODE_ENV || 'development' ;
+const pathToEnvFile = `${__dirname}/.env.${environment}`;
+require("dotenv").config({path: pathToEnvFile})
+const optionsObj = {}
+let db_name;
+let db_username;
+let db_password;
+const dialect = "dialect", host = "host", max = "max", acquire = "acquire", idle = "idle", timezone = "timezone", port = "port"
+optionsObj[dialect] = "mysql"
+
+if(environment === 'development'){
+  db_name = process.env.DEV_MYSQLDATABASE 
+  db_username = process.env.DEV_USERNAME
+  db_password = process.env.DEV_PASSWORD 
+  optionsObj[host] = process.env.DEV_HOST
+  optionsObj[max] = process.env.DEV_MAX
+  optionsObj[acquire] = process.env.DEV_ACQUIRE
+  optionsObj[idle] = process.env.DEV_IDLE
+  optionsObj[timezone] = process.env.DEV_TIMEZONE 
+  optionsObj[port] =  process.env.DEV_PORT 
+
+}else{
+    db_name = process.env.TEST_MYSQLDATABASE
+    db_username = process.env.TEST_USERNAME
+    db_password = process.env.TEST_PASSWORD 
+    optionsObj[host] = process.env.TEST_HOST
+    optionsObj[max] = process.env.TEST_MAX
+    optionsObj[acquire] = process.env.TEST_ACQUIRE 
+    optionsObj[idle] = process.env.TEST_IDLE
+    optionsObj[timezone] = process.env.TEST_TIMEZONE 
+    optionsObj[port] =  process.env.TEST_PORT 
+
+}
+
+const sequelize = new Sequelize(
+    db_name,
+    db_username,
+    db_password,
+    optionsObj
+);
+
+sequelize.authenticate().then(() => {
+   console.log('Connection has been established successfully.');
+}).catch((error) => {
+   console.error('Unable to connect to the database: ', error);
+});
---
+++ setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS items_dev_db;
+CREATE DATABASE items_dev_db;
+
+DROP DATABASE IF EXISTS items_test_db;
+CREATE DATABASE items_test_db;
